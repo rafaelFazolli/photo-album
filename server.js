230a746795bc2ba47e712ad50d37025762e6ce62
rfazolli <rafa.fazolli@hotmail.com>
2020-03-04 21:48:29 -0300

    dorenv

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
+require('dotenv/config');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/photoalbum', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 requireDir('./src/models');
 
