5e2a79e112d11296f96a9159cb229113e621c0a0
rfazolli <rafa.fazolli@hotmail.com>
2020-03-04 22:32:43 -0300

    correct

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
-const routers = require('./src/routers');
-
 require('dotenv').config();
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true  });
 
 requireDir('./src/models');
 
-app.use('/', routers);
+app.use('/api', require('./src/routers'));
 app.listen(process.env.PORT || 3001);
+
